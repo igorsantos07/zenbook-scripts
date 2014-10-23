d3a739e91ebe718a4b2b9976c65a780a01b454d6
Igor Santos <igorsantos07@gmail.com>
2014-10-23 13:19:21 -0300

    Moving folders one level above to simplify tree and fixing switch's icons path

--- ambient-light-sensor/install.sh
+++ ambient-light-sensor/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo apt-get install libbsd-dev xbacklight
-cd /opt/zenbook/ambient-light-sensor/als/controller
+sudo apt-get install libbsd-dev xbacklight qt4-qmake g++
+cd /opt/zenbook/ambient-light-sensor/controller
 git submodule init
 git submodule update
 cd als_driver
@@ -10,6 +10,6 @@ cd ../service
 qmake als-controller.pro -r -spec linux-g++-64
 make
 cd /opt/bin
-sudo ln -s /opt/zenbook/ambient-light-sensor/als/controller/als-controller
+sudo ln -s /opt/zenbook/ambient-light-sensor/controller/service/als-controller
 echo ""
 echo "########### ADD als-controller TO SYSTEM STARTUP! (/etc/rc.local)  ############"
