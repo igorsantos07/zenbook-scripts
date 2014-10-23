aea7301b6b101cb92fbb1d45530d3854f8d5ab5c
Igor Santos <igorsantos07@gmail.com>
2014-10-23 13:05:07 -0300

    Removing files and using a submodule instead (and the controller already have the driver as a submodule, so removing that as well)

--- ambient-light-sensor/als/driver/original-install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo dkms add     -m als -v master
-sudo dkms install -m als -v master
-echo als >> /etc/modules
-
-
--- ambient-light-sensor/install.sh
+++ ambient-light-sensor/install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 sudo apt-get install libbsd-dev xbacklight
-cd /opt/zenbook/ambient-light-sensor/als/driver
+cd /opt/zenbook/ambient-light-sensor/als/controller
+git submodule init
+git submodule update
+cd als_driver
 make
 sudo insmod als.ko
-cd ../controller/service
+cd ../service
 qmake als-controller.pro -r -spec linux-g++-64
 make
 cd /opt/bin
